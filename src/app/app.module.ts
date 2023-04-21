3480281785354cb54ed31c2f6947cc841714111e
Francisco Fischer <francisco.fischer92@gmail.com>
2023-04-21 13:01:14 -0300

    app.component.html cleaned, product-listing and product-details components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListingComponent } from './product-listing/product-listing.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListingComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
