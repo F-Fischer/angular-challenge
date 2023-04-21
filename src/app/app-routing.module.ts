00bc417618ad63d012240b62f47fac9c756cf0d1
Francisco Fischer <francisco.fischer92@gmail.com>
2023-04-21 17:31:55 -0300

    redirection from the item to the item detail added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListingComponent } from './product-listing/product-listing.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 const routes: Routes = [
   { path: '', component: ProductListingComponent },
+  {
+    path: ':productId-:productBrand',
+    component: ProductDetailComponent
+  }
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
