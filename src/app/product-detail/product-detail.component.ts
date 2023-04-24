cdb5ee0575df26f16baf791c88c09a19f64e6407
Francisco Fischer <francisco.fischer92@gmail.com>
2023-04-24 13:41:19 -0300

    Styles improvements on Details page

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../services/product.service';
+import { Subscription, interval, switchMap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-product-detail',
@@ -10,10 +12,15 @@ import { ProductService } from '../services/product.service';
 export class ProductDetailComponent {
   productId: any;
   productBrand: any;
+  price: any;
   product: any;
   selectedSize: any;
+  private priceSubscription: Subscription | undefined;
 
-  constructor(private route: ActivatedRoute, private productService: ProductService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService,
+    private http: HttpClient) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {  
@@ -25,6 +32,22 @@ export class ProductDetailComponent {
       // @ts-ignore
       this.product = this.productService.getProductById(parseInt(idAndBrand[0]));
       console.log(this.product);
+
+      const url = '/api/stockprice/' + this.product.skus[0].code;
+      this.priceSubscription = interval(5000)
+        .pipe(
+          switchMap(() => this.http.get(url))
+        )
+      .subscribe((data: any) => {
+        console.log(data);
+        this.price = data.price;
+      });
     });
   }
+
+  ngOnDestroy() {
+    this.priceSubscription?.unsubscribe();
+  }
+
+
 }
