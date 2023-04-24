ac2683c43e1308dd593886ca9582182fd1425d11
Francisco Fischer <francisco.fischer92@gmail.com>
2023-04-24 12:44:43 -0300

    improved styles in details page

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -16,29 +16,15 @@ export class ProductDetailComponent {
   constructor(private route: ActivatedRoute, private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      
+    this.route.paramMap.subscribe(params => {  
       let idAndBrand = params.get('productId-:productBrand')?.split('-');
       // @ts-ignore
       this.productId = idAndBrand[0];
       // @ts-ignore
       this.productBrand = idAndBrand[1];
-
       // @ts-ignore
       this.product = this.productService.getProductById(parseInt(idAndBrand[0]));
       console.log(this.product);
-
-      // const url = '/api/stockprice/' + this.product.skus[0].code;
-      // this.priceSubscription = interval(5000)
-      //   .pipe(
-      //     switchMap(() => this.http.get(url))
-      //   )
-      // .subscribe((data: any) => {
-      //   console.log(data);
-      //   this.price = data.price;
-      // });
     });
-
-    
   }
 }
